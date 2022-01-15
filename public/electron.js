44acfafc7b3313b4afbdb83d54afe9dc14e52d54
Luis J. Centeno <luisjcenteno@live.com>
2022-01-14 18:28:58 -0600

    Add electron support

---
+++ public/electron.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow, screen: electronScreen } = require('electron')
+const isDev = require('electron-is-dev');
+const path = require('path');
+
+const createMainWindow = () => {
+    let mainWindow = new BrowserWindow({
+        width: electronScreen.getPrimaryDisplay().workArea.width,
+        height: electronScreen.getPrimaryDisplay().workArea.height,
+        show: false,
+        backgroundColor: 'white',
+        webPreferences: {
+            nodeIntegration: false,
+        },
+    })
+    const startURL = 'http://localhost:3001'
+
+    mainWindow.loadURL(
+        isDev ? startURL : `file://${path.join(__dirname, '../build/index.html')}`,
+    )
+
+    mainWindow.once('ready-to-show', () => mainWindow.show())
+
+    mainWindow.on('closed', () => {
+        mainWindow = null
+    })
+}
+
+app.whenReady().then(() => {
+    createMainWindow()
+
+    app.on('activate', () => {
+        if (!BrowserWindow.getAllWindows().length) {
+            createMainWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
