c9ae27cd8a124911cea3c4cb22dc3e24ab9cee32
Luis J. Centeno <luisjcenteno@live.com>
2022-01-31 20:03:32 -0600

    📦 Remove unused packages

--- src/utils/services/index.ts
+++ src/utils/services/index.ts
@@ -1,9 +1,5 @@
-import API_ROUTES from 'app-constants/api-routes'
 import Cookies from 'js-cookie'
-import HTTPClient from 'utils/http-client'
 import moment from 'moment'
-import { TAsset } from 'types/asset'
-import { AxiosResponse } from 'axios'
 import { TFormSet } from 'types/machinery'
 
 export const getAppName = (): string | undefined => process.env.REACT_APP_WEBSITE_NAME
@@ -32,22 +28,6 @@ export const cleanUserSession = (): void => {
     Cookies.remove('authToken')
     Cookies.remove('tokenExpiration')
     Cookies.remove('user')
-}
-
-export const assetUpload = async (asset: File): Promise<TAsset[] | undefined> => {
-    try {
-        const formData = new FormData()
-        formData.append('files', asset)
-
-        const response = await HTTPClient.post<FormData, AxiosResponse<TAsset[] | undefined>>(
-            API_ROUTES.ASSETS.ROOT,
-            formData,
-        )
-        return response.data
-    } catch (error) {
-        console.error(error)
-        return undefined
-    }
 }
 
 export const getAssetPath = (relativePath?: string): string => {
